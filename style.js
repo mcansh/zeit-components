afc23186c2de16dc90e764a645e1e89237a76728
Logan McAnsh <logan@mcan.sh>
2019-07-31 20:58:32 -0400

    wipe all the things
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- style.js
+++
@@ -1,7 +0,0 @@
-export default {
-  code: '#bd10e0',
-  error: '#ff001f',
-  links: '#067df7',
-  monospaced:
-    'Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif',
-};
